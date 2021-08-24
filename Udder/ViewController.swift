8e1ab77ba0b11cc4f00741cf1550b963fa4a24b4
Fabian Ekbäck <fabian.ekback@gmail.com>
2021-08-24 11:37:58 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Fabian Ekbäck on 2021-08-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
